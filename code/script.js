35eccaf56d0014ae0480462b757c59686911c568
Erin Le <jianghuale@gmail.com>
2024-03-04 19:46:27 -0800

    skeleton code

---
+++ code/script.js
@@ -0,0 +1,19 @@
+
+// put code for stem/guided narrative here: parallel plot
+
+//put labels of axes we want to plot here
+axis_names = []
+
+y_axis_scalers = []
+
+
+
+
+// code for parallel brushable plot
+
+// code for bubble
+
+// code for cluster
+
+// whatever js code needed for scikit
+

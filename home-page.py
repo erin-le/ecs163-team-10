531f0676ea7f1dcdedbe7a5e63e174973c217e61
Kris Wong <37485546+SerpentSnek@users.noreply.github.com>
2024-03-06 19:58:47 -0800

    made a neural net (it is much better)

--- home-page.py
+++ home-page.py
@@ -1,14 +1,15 @@
 import numpy as np
 import joblib
+import sys
 
 from flask import Flask, request, render_template
 # use socketIO to async update the form div
-from flask_socketio import SocketIO, emit
+# from flask_socketio import SocketIO, emit
 
 app = Flask(__name__, template_folder='code')
 
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+# app.config['SECRET_KEY'] = 'secret!'
+# socketio = SocketIO(app)
 
 @app.route('/')
 def home():
@@ -18,17 +19,16 @@ def home():
 def ml():
   return render_template('ml-interaction/index.html')
 
-model = joblib.load('ml-models/bi-logistic-reg.pkl')
+model = joblib.load('ml-models/logistic-reg')
 
 @app.route('/ml-interact', methods = ['POST'])
-@socketio.on('submit') 
+# @socketio.on('submit') 
 def predict():
     
     #obtain all form values and place them in an array, convert into integers
     int_features = [int(x) for x in request.form.values()]
     # #Combine them all into a final numpy array
     final_features = [np.array(int_features)]
-    # final_features = [np.array(request.form.values())].reshape(1,-1)
     #predict the price given the values inputted by user
     prediction = model.predict(final_features)
     
@@ -38,6 +38,8 @@ def predict():
     #Remember target output mapping: dropout=1, graduate=3
     if output == 1:
         return render_template('ml-interaction/index.html', prediction_text = "Dropout")
+    elif output == 2:
+        return render_template('ml-interaction/index.html', prediction_text = "Enrolled (too early to say)")
     elif output == 3:
         return render_template('ml-interaction/index.html', prediction_text = "Graduate")  
 
